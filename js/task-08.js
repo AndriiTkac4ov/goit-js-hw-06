fdd1131d01d62d0061c19b92ccce6a46b1322084
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2022-10-08 13:49:17 +0300

    solve task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,38 @@
+const loginFormEl = document.querySelector('.login-form');
 
+loginFormEl.addEventListener('submit', onLoginForm);
+
+function onLoginForm(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+    const formData = {
+        email,
+        password,
+    };
+
+    console.log(formData);
+
+    if (email === '' || password === '') {
+        alert ('Please fill in all the fields!')
+    }
+
+    event.currentTarget.reset();
+
+    // ==========================
+
+    // const formData = new FormData(event.currentTarget);
+
+    // // console.log(formData);
+    // const formObject = {};
+    // formData.forEach((value, name) => {
+    //     // console.log(`${name}: ${value}`);
+    //     // formObject[name] = name;
+    //     formObject[name] = value;
+    //     console.log(formObject);
+    // });
+
+    // ==========================
+};
