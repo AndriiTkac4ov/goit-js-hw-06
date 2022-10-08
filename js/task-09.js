29a55bfd0000ab0045ce1c070048848e004a37a8
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2022-10-08 14:12:25 +0300

    start task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.body;
+const backgroundColorTextEl = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+
+bodyEl.style.backgroundColor = 'red';
+backgroundColorTextEl.addEventListener('change', getRandomHexColor);
+changeColorBtn.addEventListener('click', getRandomHexColor);
+
+console.log(backgroundColorTextEl);
+console.log(changeColorBtn);
