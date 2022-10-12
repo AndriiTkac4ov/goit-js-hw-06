3195df82dd9bd6cc1332f3293b325f7dd29715d2
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2022-10-12 21:46:42 +0300

    upgrade task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,18 +1,30 @@
-const categoriesItemsEl = document.querySelectorAll('.item');
+// const categoriesItemsEl = document.querySelectorAll('.item');
 
-// 1)
-const countCategories = function (categories) {
-    console.log(`Number of categories: ${categories.length}`);
-};
+// // 1)
+// const countCategories = function (categories) {
+//     console.log(`Number of categories: ${categories.length}`);
+// };
 
-countCategories(categoriesItemsEl);
+// countCategories(categoriesItemsEl);
 
-// 2)
-const findCategoryWithElements = function (categories) {
-    for (const category of categories) {
-        console.log(`Category: ${category.firstElementChild.textContent}`);
-        console.log(`Elements: ${category.lastElementChild.children.length}`);
-    };
-};
+// // 2)
+// const findCategoryWithElements = function (categories) {
+//     for (const category of categories) {
+//         console.log(`Category: ${category.firstElementChild.textContent}`);
+//         console.log(`Elements: ${category.lastElementChild.children.length}`);
+//     };
+// };
 
-findCategoryWithElements(categoriesItemsEl);
+// findCategoryWithElements(categoriesItemsEl);
+
+// ============================
+
+const list = document.querySelector('#categories');
+const items = list.querySelectorAll('.item');
+console.log(`В списку ${items.length} категорії:`);
+
+items.forEach(node => {
+    const h2 = node.querySelector('h2');
+    const nodeItems = node.querySelectorAll('li');
+    console.log(` - категорія ${h2.textContent} (кількість елементів: ${nodeItems.length})`);
+});
